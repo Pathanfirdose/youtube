845eea2f25101537d0ca195e0666bd7fc4ac8aa7
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 15:25:24 +0530

    shell

--- shell/18-while-demo.sh
+++ shell/18-while-demo.sh
@@ -3,8 +3,9 @@
 count=0
 num=10
 
-while [ $count -le $num ]
+while [ $count -le $num ] # it runs until count is less than equal to num that till its true it runs once it is fales then stops
 do
     echo "value of count var is $count"
-    let count++
+    let count++ # every time it increase the value of count 0+1 0+2 0+3
 done
+
