3a70893d4f44869dca28c4108aa11a43a5550a3c
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 16:13:36 +0530

    shell

--- shell/24-function-demo.sh
+++ shell/24-function-demo.sh
@@ -10,3 +10,14 @@ function myfunc {
 myfunc
 myfunc
 myfunc
+
+myfunc1() {
+    
+    echo "1----------------------"
+    echo "2-------welcome--------"
+    echo "3----------------------"
+}
+
+myfunc1
+myfunc1
+myfunc1
