f762317387b83bf51ca982fc8dae1a6288c38418
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 23:46:47 +0530

    shell

--- shell/25-function-with-org.sh
+++ shell/25-function-with-org.sh
@@ -4,9 +4,10 @@ function myfunc {
     
     echo "----------------------"
     echo "-------welcome $1-----"
+    echo "-------age $2---------"
     echo "----------------------"
 }
 
-myfunc sabir
-myfunc riyaz
-myfunc arshad
+myfunc sabir 28
+myfunc riyaz 32 
+myfunc arshad 40
