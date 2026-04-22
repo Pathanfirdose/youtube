d46b6157ee4e940e744f4e7d528d9ec8ec21d370
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 11:35:52 +0530

    shell

--- shell/12-choice.sh
+++ shell/12-choice.sh
@@ -8,8 +8,8 @@ echo "c for list of files and directories"
 read choice
 
 case $choice
-    a)date;;
-    b)pwd;;
-    c)ls;;
-    *)echo "enter valid value"
+        a)date;;
+        b)pwd;;
+        c)ls;;
+        *)echo "enter valid value"
 esac
