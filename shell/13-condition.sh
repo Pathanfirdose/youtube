faedcb155eb50bb5a613eda46e8b73bd69f5a033
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 11:54:26 +0530

    shell

--- shell/13-condition.sh
+++ shell/13-condition.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-# read -p "enter your age" age
-# read -p "enter your country" country
+read -p "enter your age" age
+read -p "enter your country" country
 
-# if [ $age -gt 40 ] && [ $country -eq "india"] # && and condition
-# then
-#     echo "you can vote"
-# else
-#     echo "you cant vote"
-# fi
+if [ $age -gt 40 ] && [ $country -eq "india" ] # && and condition
+then
+    echo "you can vote"
+else
+    echo "you cant vote"
+fi
 
 read -p "enter your age" age
 read -p "enter your country" country
 
-if [ $age -gt 40 ] || [ $country == "india"] # || or condition
+if [ $age -gt 40 ] || [ $country -eq "india" ] # || or condition
 then
     echo "you can vote"
 else
