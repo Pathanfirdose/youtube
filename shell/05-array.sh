ff75d1c5e3527f5b12771a510d8e816b9c2a8f59
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 08:34:04 +0530

    shell

--- shell/05-array.sh
+++ shell/05-array.sh
@@ -7,3 +7,8 @@ myarray=(1 30 50 firdose "ghouse khan")
 echo "this is my array value in third index ${myarray[2]}"
 
 echo "all values in it ${myarray[@]}"
+
+# how  to get lenght of array
+
+echo "no of arrays in this ${#myarray[@]}"
+echo "no of arrays in this ${myarray[#@]}"
