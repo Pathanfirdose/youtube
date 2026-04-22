d9d53babdf25eb144e8d63c8232ecccb9e1b8a1b
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 09:04:07 +0530

    shell

--- shell/07-string-ops.sh
+++ shell/07-string-ops.sh
@@ -5,6 +5,7 @@ myvar="hey sabir how are you"
 myvarlength=${#myvar}
 
 echo "length of my vars $myvarlength"
+echo "length of my vars ${#myvar}"
 
 echo "upper case is ----- ${myvar^^}"
 echo "lower case is ----- ${myvar,,}"
