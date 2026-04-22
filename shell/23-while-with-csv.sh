718e8d7ffde14d169eb0ced71f3416a913f9ea51
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 16:03:15 +0530

    shell

--- shell/23-while-with-csv.sh
+++ shell/23-while-with-csv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cat details.csv | awk 'NR!=1 {print}' | while IFS="," read id name age # this means to exclude first line
+cat details.csv | awk 'NR!=1 {print}' |while IFS="," read id name age # this means to exclude first line
 do
     echo "id is $id"
     echo "name is $name"
