4725f062623f53125f81ed912f4ae3558cf20795
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-22 11:41:15 +0530

    shell

--- shell/12-choice.sh
+++ shell/12-choice.sh
@@ -13,3 +13,21 @@ case $choice in
         c)ls;;
         *)echo "enter valid value"
 esac
+
+echo "provide an option"
+echo "a for date"
+echo "b for current working directory"
+echo "c for list of files and directories"
+
+read choice
+
+case $choice in
+        a)      
+                echo "today date is .."
+                date
+                echo "end..."
+                ;;
+        b)pwd;;
+        c)ls;;
+        *)echo "enter valid value"
+esac
