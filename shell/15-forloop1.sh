f0983c4e62b482b5d6e17573cb40f564feb7a4dc
Pathanfkhan <p.firdosekhan@gmail.com>
2026-04-25 16:19:11 +0530

    shell

--- shell/15-forloop1.sh
+++ shell/15-forloop1.sh
@@ -3,9 +3,12 @@
 for i in 1 2 3 4 5 6 7 8 9
 do
     echo "no. is $i"
+    sleep 3s
 done
 
 for names in firdose sabir riyaz "arshad ali"
 do
     echo "my friend is $names"
 done
+
+# whene ever you want to run script in background use command: nohup ./15-forloop1.sh 
