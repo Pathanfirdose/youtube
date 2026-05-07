ce32d6c112585cf3ed478fbe4b993633ac6265d9
Pathanfirdose <pfirdose1988@gmail.com>
2026-05-07 07:27:07 +0530

    shell

--- shell/projects/user_make.sh
+++ shell/projects/user_make.sh
@@ -16,3 +16,44 @@ then
 fi
 
 # store first argument as username
+USER_NAME=${1}
+echo $USER_NAME
+
+# shit comment
+shift
+COMMENT=${@}
+
+#CREATE PASSWORD
+PASSWORD=$(date +%s%N)
+echo $PASSWORD
+
+# create the user
+useradd -c "about user" -m $USER_NAME
+
+#check user creation
+if [ ${?} -ne 0 ]
+then
+    echo user not created
+    exit 1
+fi
+
+# ste password for user
+echo $PASSWORD | passwd --stdin $USER_NAME
+
+# check password correctly set
+if [ ${?} -ne 0 ]
+then
+    echo password not set
+    exit 1
+fi
+
+# force password change on first login
+passwd -e $USER_NAME
+
+# display username,host and other
+echo
+echo username: $USER_NAME
+echo
+echo password: $PASSWORD
+echo 
+echo hostname is $(hostname)
